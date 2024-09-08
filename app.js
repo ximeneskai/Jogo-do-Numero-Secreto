0983c58859d5c2bb4480ca3af963fb40ce36e137
Kai <kaiximenes@outlook.com>
2024-09-08 15:45:00 -0300

    Alteração de limite do Número Secreto
    Limite anterior era de 1 a 10; Novo limite é de 1 a 100.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // let titulo = document.querySelector('h1');
 // titulo.innerHTML = 'Jogo do número secreto';
 // let paragrafo = document.querySelector('p');
-// paragrafo.innerHTML = 'Escolha um número entre 1 e 10:';
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 100:';
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10
+let numeroLimite = 100
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -17,7 +17,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto'); 
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10:');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100:');
 }
 
 exibirMensagemInicial()
